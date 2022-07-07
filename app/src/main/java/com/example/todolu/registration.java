7b79f4ed7adf08c1eeaf50a7de10a3a68a040c11
gurmehrkaur <gurmehrkaur@gurmehrs-macbook-air.local>
2022-07-07 16:53:45 -0700

    Initial commit

---
+++ app/src/main/java/com/example/todolu/registration.java
@@ -0,0 +1,14 @@
+package com.example.todolu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+    }
+}
