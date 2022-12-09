6b4e60b071c0db7f139df1dc4c03e40270816d9e
antdoan123 <aqdoan@cpp.edu>
2022-12-09 00:50:42 -0800

    working on music UI

--- app/src/main/java/com/example/todolu/fragments/Home.java
+++ app/src/main/java/com/example/todolu/fragments/Home.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
